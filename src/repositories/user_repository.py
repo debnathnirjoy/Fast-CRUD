4ef5568d7b726902d708d5d7e2c6f144793733f7
Nirjoy Debnath <nirjoy.debnath@cefalo.com>
2024-09-24 16:42:56 +0600

    Implemented user service

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from src.db.models.user import User
+from src.schemas.user import UserCreateSchema, UserUpdateSchema
+
+def create_user(db: Session, user: UserCreateSchema):
+    new_user = User(username=user.name, email=user.email, password=user.password, name=user.name)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def get_all_users(db: Session):
+    users = db.query(User).all()
+    return users
+
+def get_user_by_id(db: Session, user_id: str):
+    user = db.query(User).filter_by(id=user_id).first()
+    return user
+
+def update_user_by_id(db: Session, user_id: str, updated_user: UserUpdateSchema):
+    updated_user_dict = updated_user.model_dump(exclude_unset=True)
+    update_count = db.query(User).filter_by(id=user_id).update(updated_user_dict)
+    db.commit()
+    return update_count
+
+def delete_user_by_id(db: Session, user_id: str):
+    delete_count = db.query(User).filter_by(id=user_id).delete()
+    db.commit()
+    return delete_count
---
+++ src/services/user_service.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from src.repositories import user_repository
+from src.schemas.user import UserCreateSchema, UserUpdateSchema
+from fastapi import status, HTTPException
+
+
+def create_user(db: Session, user: UserCreateSchema):
+    return user_repository.create_user(db, user)
+
+def get_all_users(db: Session):
+    return user_repository.get_all_users(db)
+
+def get_user_by_id(db: Session, user_id: str):
+    return user_repository.get_user_by_id(db, user_id)
+
+def update_user_by_id(db: Session, user_id: str, updated_user: UserUpdateSchema):
+    update_count = user_repository.update_user_by_id(db, user_id, updated_user)
+    if update_count > 0:
+        return updated_user.model_dump()
+    else:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+
+def delete_user_by_id(db: Session, user_id: str):
+    delete_count = user_repository.delete_user_by_id(db, user_id)
+    if delete_count > 0:
+        return delete_count
+    else:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
